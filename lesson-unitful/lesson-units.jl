5fc875ea2a9db8fc8b70ccfadf5b8216bf761039
Aurelio Amerio <aure.amerio@gmail.com>
2019-12-26 15:15:24 +0100

    added lesson units

--- lesson-unitful/lesson-units.jl
+++ lesson-unitful/lesson-units.jl
@@ -9,13 +9,31 @@ b
 one_meter
 
 c = ustrip(u"m", one_meter)
+c
+typeof(c)
+one_meter
+
+ustrip(u"km", one_meter)
+ustrip(one_meter)
+#%%
+function compute_speed(Δx, Δt)
+    return Δx/Δt
+end
+
+compute_speed(1u"km", 2u"s")
 
 function compute_speed(Δx::Unitful.Length, Δt::Unitful.Time)
     return uconvert(u"m/s", Δx/Δt)
 end
 
-typeof(compute_speed(1u"km", 2u"s"))
+compute_speed(1u"km", 2u"s")
+#%%
+struct Person
+    height::typeof(1.0u"m")
+    mass::typeof(1.0u"kg")
+end
+#%%
+using QuadGK
+velocity(t::Unitful.Time) = 2u"m/s^2"*t + 1u"m/s"
 
-velocity(t::Unitful.Time) = 2u"m/s^2"*t
-
-quadgk(velocity, 0u"s", 3u"s")
+quadgk(velocity, 0u"s", 3u"s")[1]
